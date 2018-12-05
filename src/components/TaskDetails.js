a3aac00ff419a9ba460077895f798147a30443a4
isabel985 <lee.isabel59@gmail.com>
2018-12-04 20:32:34 -0600

    added functionality of button more

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const TaskDetails = (props) => {
     return (
+        <div className="details-sidebar">
         <h1>{props.selectedTask.task_name}</h1>
+        </div>
     );
 }
 
--- src/containers/TasksPage.js
+++ src/containers/TasksPage.js
@@ -31,37 +31,33 @@ class TasksPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="table-headers">
-          <h1>Tasks</h1>
-          <span>Due Date</span>
-          <span>Project Name</span>
+        <div className={ this.state.selectedTask ? `summaries-list sidebar-expanded` : `summaries-list` }>
+          <div className="table-headers">
+            <h1>Tasks</h1>
+            <span>Due Date</span>
+            <span>Project Name</span>
+          </div>
+          <ul className="list">
+            {data.map( (project)  => {
+              return project.project_tasks.map( (task) => {
+                return <TaskSummary 
+                  key={task.task_id} 
+                  task={task}
+                  project_id={project.project_id}
+                  project_name={project.project_name}
+                  handleDisplayTaskDetails={this.handleDisplayTaskDetails}
+                />
+              })
+            })}
+          </ul>
         </div>
-        <ul className="list">
-          {data.map( (project)  => {
-            return project.project_tasks.map( (task) => {
-              return <TaskSummary 
-                key={task.task_id} 
-                task={task}
-                project_id={project.project_id}
-                project_name={project.project_name}
-                handleDisplayTaskDetails={this.handleDisplayTaskDetails}
-              />
-            })
-          })}
-        </ul>
-
-        {/* sidebar goes here */}
 
         {console.log(this.state.selectedTask)}
-
-        {/* if this.state.selectedTask then render the sidebar component */}
 
         {this.state.selectedTask && <TaskDetails selectedTask={this.state.selectedTask}/>}
       </React.Fragment>
     );
   }
 }
-
-// return <TaskSummary key={project.project_id} task_name={project.project_name} task_due_date={0}/>
 
 export default TasksPage;
