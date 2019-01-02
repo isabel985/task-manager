d184b025fe0a894c5d7153ecf561bca180aa22b3
isabel985 <lee.isabel59@gmail.com>
2019-01-02 17:49:12 -0600

    team members page basic structure created

--- src/containers/TeamMembersPage.js
+++ src/containers/TeamMembersPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import teammemberdata from '../data/teammembersdata';
 
 class TeamMembersPage extends Component {
   constructor(props) {
@@ -7,7 +8,17 @@ class TeamMembersPage extends Component {
 
   render() {
     return (
-      <h1>Team Members</h1>
+      <React.Fragment> 
+        <div className="summaries-list team-members">
+          <div className="table-headers">
+            <h1>Team Members</h1>
+          </div>
+          <ul className="list">
+            <li className="list-item">Hal</li>
+            <li className="list-item">Hatem</li>
+          </ul>
+        </div>
+      </React.Fragment>
     )
   }
 
---
+++ src/data/teammembersdata.js
@@ -0,0 +1,20 @@
+let teamMembers = [
+  {
+      member_id: "1",
+      member_name: "Jim"
+  },
+  {
+      member_id: "2",
+      member_name: "Dwight"
+  },
+  {
+      member_id: "3",
+      member_name: "Pam"
+  },
+  {
+      member_id: "4",
+      member_name: "Michael"
+  }
+];
+
+export default teamMembers;
